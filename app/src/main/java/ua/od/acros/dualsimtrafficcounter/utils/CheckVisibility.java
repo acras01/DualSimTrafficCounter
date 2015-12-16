05ee273ff4fdbe20a43a8d930d14d449291e230c
acras <acras1@gmail.com>
2015-12-16 16:15:13 +0200

    14/12

--- app/src/main/java/ua/od/acros/dualsimtrafficcounter/utils/CheckVisibility.java
+++ app/src/main/java/ua/od/acros/dualsimtrafficcounter/utils/CheckVisibility.java
@@ -2,7 +2,28 @@ package ua.od.acros.dualsimtrafficcounter.utils;
 
 import android.app.Application;
 
+import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+import ua.od.acros.dualsimtrafficcounter.R;
+
+@ReportsCrashes(mailTo = "acras1@gmail.com",
+        customReportContent = { ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME,
+                ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL, ReportField.SETTINGS_GLOBAL, ReportField.SETTINGS_SYSTEM,
+                ReportField.STACK_TRACE, ReportField.LOGCAT, ReportField.SHARED_PREFERENCES },
+        logcatArguments = { "-t", "200", "-v", "long", "*:W" },
+        mode = ReportingInteractionMode.TOAST,
+        resToastText = R.string.crash_toast_text)
 public class CheckVisibility extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // The following line triggers the initialization of ACRA
+        ACRA.init(this);
+    }
 
     public static boolean isActivityVisible() {
         return activityVisible;
