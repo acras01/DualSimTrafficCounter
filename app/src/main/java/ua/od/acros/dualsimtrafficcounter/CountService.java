bb6d682048134f21881ff627b9530feca685cfc7
acras <acras1@gmail.com>
2016-01-14 10:01:54 +0200

    13/01

--- app/src/main/java/ua/od/acros/dualsimtrafficcounter/CountService.java
+++ app/src/main/java/ua/od/acros/dualsimtrafficcounter/CountService.java
@@ -1526,9 +1526,9 @@ public class CountService extends Service implements SharedPreferences.OnSharedP
             if (simNumber >= 1)
                 text = DataFormat.formatData(context, isNight1 ? (long) dataMap.get(Constants.TOTAL1_N) : (long) dataMap.get(Constants.TOTAL1));
             if (simNumber >= 2)
-                text += "||" + DataFormat.formatData(context, isNight2 ? (long) dataMap.get(Constants.TOTAL2_N) : (long) dataMap.get(Constants.TOTAL2));
+                text += " || " + DataFormat.formatData(context, isNight2 ? (long) dataMap.get(Constants.TOTAL2_N) : (long) dataMap.get(Constants.TOTAL2));
             if (simNumber == 3)
-                text += "||" + DataFormat.formatData(context, isNight3 ? (long) dataMap.get(Constants.TOTAL3_N) : (long) dataMap.get(Constants.TOTAL3));
+                text += " || " + DataFormat.formatData(context, isNight3 ? (long) dataMap.get(Constants.TOTAL3_N) : (long) dataMap.get(Constants.TOTAL3));
         } else {
             switch (sim) {
                 case Constants.SIM1:
