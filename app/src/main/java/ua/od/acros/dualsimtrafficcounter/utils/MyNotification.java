ee75beb6889a1afab36f0894851024c33f1e71a6
acras <acras1@gmail.com>
2016-02-02 12:21:55 +0200

    02/02

--- app/src/main/java/ua/od/acros/dualsimtrafficcounter/utils/MyNotification.java
+++ app/src/main/java/ua/od/acros/dualsimtrafficcounter/utils/MyNotification.java
@@ -9,14 +9,19 @@ import ua.od.acros.dualsimtrafficcounter.R;
 public class MyNotification extends Notification {
 
     private static String mTraffic, mCalls;
+    private static final String XPOSED = "de.robv.android.xposed.installer";
 
     public static Notification build(NotificationCompat.Builder builder, Context context, String traffic, String calls) {
         if (traffic.equals(""))
             traffic = mTraffic;
         if (calls.equals(""))
             calls = mCalls;
-        String bigText = context.getString(R.string.traffic)  + "\n" + traffic + "\n" +
-                context.getString(R.string.calls) + "\n" + calls + "\n";
+        String bigText;
+        if (XposedUtils.isPackageExisted(context, XPOSED))
+            bigText = context.getString(R.string.traffic)  + "\n" + traffic + "\n" +
+                    context.getString(R.string.calls) + "\n" + calls + "\n";
+        else
+            bigText = context.getString(R.string.traffic)  + "\n" + traffic;
         mTraffic = traffic;
         mCalls = calls;
         return new NotificationCompat.BigTextStyle(builder)
