fda804a88aef05d93827d8c39b02fdf99aa03921
acras <acras1@gmail.com>
2016-02-03 09:09:18 +0200

    03/02

--- app/src/main/java/ua/od/acros/dualsimtrafficcounter/utils/CheckServiceRunning.java
+++ app/src/main/java/ua/od/acros/dualsimtrafficcounter/utils/CheckServiceRunning.java
@@ -3,27 +3,18 @@ package ua.od.acros.dualsimtrafficcounter.utils;
 import android.app.ActivityManager;
 import android.content.Context;
 
-import java.util.List;
-
 public class CheckServiceRunning {
 
     public static boolean isMyServiceRunning(Class<?> serviceClass, Context context) {
-        ActivityManager manager;
-        if (context != null)
-            manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        else
+        try {
+            ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+                if (serviceClass.getName().equals(service.service.getClassName()))
+                    return true;
+            }
+        } catch (Exception e) {
             return false;
-        if (manager != null) {
-            List<ActivityManager.RunningServiceInfo> list = manager.getRunningServices(Integer.MAX_VALUE);
-            if (list != null && list.size() > 0)
-                for (ActivityManager.RunningServiceInfo service : list) {
-                    if (serviceClass.getName().equals(service.service.getClassName()))
-                        return true;
-                }
-            else
-                return false;
-        } else
-            return false;
+        }
         return false;
     }
 
