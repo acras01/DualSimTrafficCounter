7cce105f7fe64c665f067ce02b67238f53679ac1
acras <acras1@gmail.com>
2016-02-29 10:46:49 +0200

    29/02

--- app/src/main/java/ua/od/acros/dualsimtrafficcounter/MainActivity.java
+++ app/src/main/java/ua/od/acros/dualsimtrafficcounter/MainActivity.java
@@ -82,7 +82,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolBar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
