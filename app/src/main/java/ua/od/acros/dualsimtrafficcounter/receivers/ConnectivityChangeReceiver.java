f6ab3cf039e03eb3769c1cddf273b3d03ce4bac2
acras <ZqwErtY1981>
2016-05-31 14:18:24 +0300

    31/05

--- app/src/main/java/ua/od/acros/dualsimtrafficcounter/receivers/ConnectivityChangeReceiver.java
+++ app/src/main/java/ua/od/acros/dualsimtrafficcounter/receivers/ConnectivityChangeReceiver.java
@@ -23,27 +23,29 @@ public class ConnectivityChangeReceiver extends BroadcastReceiver {
         SharedPreferences prefs = context.getSharedPreferences(Constants.APP_PREFERENCES, Context.MODE_PRIVATE);
         boolean floatingWindow = prefs.getBoolean(Constants.PREF_OTHER[32], false);
         boolean alwaysShow = !prefs.getBoolean(Constants.PREF_OTHER[41], false);
-        if (intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, Boolean.FALSE)) {
-            if (floatingWindow && alwaysShow)
-                FloatingWindowService.closeFloatingWindow(context, prefs);
-            if (CustomApplication.isMyServiceRunning(TrafficCountService.class))
-                EventBus.getDefault().post(new NoConnectivityEvent());
-        } else {
-            if (MobileUtils.hasActiveNetworkInfo(context) == 2) {
-                if (floatingWindow && alwaysShow)
-                    FloatingWindowService.showFloatingWindow(context, prefs);
-                if (!CustomApplication.isMyServiceRunning(TrafficCountService.class) &&
-                        !prefs.getBoolean(Constants.PREF_OTHER[5], false)) {
-                    Intent i = new Intent(context, TrafficCountService.class);
-                    i.setAction(intent.getAction());
-                    i.putExtras(intent.getExtras());
-                    i.setFlags(intent.getFlags());
-                    context.startService(i);
-                } else if (CustomApplication.isMyServiceRunning(TrafficCountService.class))
-                    EventBus.getDefault().post(new MobileConnectionEvent());
-            } else {
-                if (floatingWindow && alwaysShow)
+        if (intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
+            if (intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, Boolean.FALSE)) {
+                if (floatingWindow && !alwaysShow)
                     FloatingWindowService.closeFloatingWindow(context, prefs);
+                if (CustomApplication.isMyServiceRunning(TrafficCountService.class))
+                    EventBus.getDefault().post(new NoConnectivityEvent());
+            } else {
+                if (MobileUtils.hasActiveNetworkInfo(context) == 2) {
+                    if (floatingWindow && alwaysShow)
+                        FloatingWindowService.showFloatingWindow(context, prefs);
+                    if (!CustomApplication.isMyServiceRunning(TrafficCountService.class) &&
+                            !prefs.getBoolean(Constants.PREF_OTHER[5], false)) {
+                        Intent i = new Intent(context, TrafficCountService.class);
+                        i.setAction(intent.getAction());
+                        i.putExtras(intent.getExtras());
+                        i.setFlags(intent.getFlags());
+                        context.startService(i);
+                    } else if (CustomApplication.isMyServiceRunning(TrafficCountService.class))
+                        EventBus.getDefault().post(new MobileConnectionEvent());
+                } else {
+                    if (floatingWindow && !alwaysShow)
+                        FloatingWindowService.closeFloatingWindow(context, prefs);
+                }
             }
         }
     }
