8a4417185134507831c96d1fa6deed7b5db6e6c0
acras <ZqwErtY1981>
2016-08-10 09:05:19 +0300

    10/08

---
+++ app/src/main/java/ua/od/acros/dualsimtrafficcounter/dialogs/ManualSimDialog.java
@@ -0,0 +1,79 @@
+package ua.od.acros.dualsimtrafficcounter.dialogs;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+
+import ua.od.acros.dualsimtrafficcounter.R;
+import ua.od.acros.dualsimtrafficcounter.utils.Constants;
+import ua.od.acros.dualsimtrafficcounter.utils.CustomApplication;
+
+public class ManualSimDialog extends AppCompatActivity {
+
+    private static boolean mIsActive;
+    private Dialog mDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mIsActive = true;
+        final Context context = CustomApplication.getAppContext();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        if (savedInstanceState == null) {
+            if (prefs.getBoolean(Constants.PREF_OTHER[29], true))
+                getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
+            else {
+                if (prefs.getBoolean(Constants.PREF_OTHER[28], false))
+                    getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                else
+                    getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+            // Now recreate for it to take effect
+            recreate();
+        }
+
+        mDialog = new AlertDialog.Builder(this)
+                .setTitle(R.string.attention)
+                .setMessage(R.string.sim_not_defined)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog1, int id) {
+                        dialog1.cancel();
+                    }
+                })
+                .create();
+
+        if (!this.isFinishing()) {
+            mDialog.show();
+        }
+    }
+
+    public static boolean isActive() {
+        return mIsActive;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mIsActive = true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mIsActive = false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mIsActive = false;
+        if (mDialog != null && mDialog.isShowing())
+            mDialog.dismiss();
+    }
+}
--- app/src/main/java/ua/od/acros/dualsimtrafficcounter/services/TrafficCountService.java
+++ app/src/main/java/ua/od/acros/dualsimtrafficcounter/services/TrafficCountService.java
@@ -47,6 +47,7 @@ import ua.od.acros.dualsimtrafficcounter.R;
 import ua.od.acros.dualsimtrafficcounter.activities.SettingsActivity;
 import ua.od.acros.dualsimtrafficcounter.dialogs.ChooseActionDialog;
 import ua.od.acros.dualsimtrafficcounter.dialogs.ChooseSimDialog;
+import ua.od.acros.dualsimtrafficcounter.dialogs.ManualSimDialog;
 import ua.od.acros.dualsimtrafficcounter.events.ActionTrafficEvent;
 import ua.od.acros.dualsimtrafficcounter.events.MobileConnectionEvent;
 import ua.od.acros.dualsimtrafficcounter.events.NoConnectivityEvent;
@@ -224,7 +225,14 @@ public class TrafficCountService extends Service implements SharedPreferences.On
         }
         if (mPrefs.getBoolean(Constants.PREF_OTHER[43], true)) {
             mActiveSIM = MobileUtils.getActiveSimForData(mContext);
-            timerStart(Constants.COUNT);
+            if (mActiveSIM != Constants.DISABLED)
+                timerStart(Constants.COUNT);
+            else {
+                Intent dialogIntent = new Intent(mContext, ManualSimDialog.class);
+                dialogIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                if (!ManualSimDialog.isActive())
+                    mContext.startActivity(dialogIntent);
+            }
         } else {
             Intent dialogIntent = new Intent(mContext, ChooseSimDialog.class);
             dialogIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
