501991fe8fb8464e1f4ab512b884d9a6074e732e
acras <ZqwErtY1981>
2018-02-22 17:00:11 +0200

    Logo fix

--- app/src/main/java/ua/od/acros/dualsimtrafficcounter/receivers/BootBroadcastReceiver.java
+++ app/src/main/java/ua/od/acros/dualsimtrafficcounter/receivers/BootBroadcastReceiver.java
@@ -17,37 +17,40 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
     @Override
     public final void onReceive(Context context, Intent intent) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-
-        //start CountService
-        if (!prefs.getBoolean(Constants.PREF_OTHER[5], false) && !prefs.getBoolean(Constants.PREF_OTHER[47], false))
-            context.startService(new Intent(context, TrafficCountService.class));
-        else {
-            //Update widgets
-            int[] ids = CustomApplication.getWidgetIds(Constants.TRAFFIC);
-            if (ids.length != 0) {
-                Intent i = new Intent(Constants.TRAFFIC_BROADCAST_ACTION);
-                i.putExtra(Constants.WIDGET_IDS, ids);
-                context.sendBroadcast(i);
+        if (intent.getAction() != null &&
+                (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)
+                        || intent.getAction().equals("android.intent.action.QUICKBOOT_POWERON")
+                        || intent.getAction().equals("com.htc.intent.action.QUICKBOOT_POWERON"))) {
+            //start CountService
+            if (!prefs.getBoolean(Constants.PREF_OTHER[5], false) && !prefs.getBoolean(Constants.PREF_OTHER[47], false))
+                context.startService(new Intent(context, TrafficCountService.class));
+            else {
+                //Update widgets
+                int[] ids = CustomApplication.getWidgetIds(Constants.TRAFFIC);
+                if (ids.length != 0) {
+                    Intent i = new Intent(Constants.TRAFFIC_BROADCAST_ACTION);
+                    i.putExtra(Constants.WIDGET_IDS, ids);
+                    context.sendBroadcast(i);
+                }
             }
-        }
-        //start CallLoggerService
-        if (!prefs.getBoolean(Constants.PREF_OTHER[24], false))
-            context.startService(new Intent(context, CallLoggerService.class));
-        else {
-            //Update widgets
-            int[] ids = CustomApplication.getWidgetIds(Constants.CALLS);
-            if (ids.length != 0) {
-                Intent i = new Intent(Constants.CALLS_BROADCAST_ACTION);
-                i.putExtra(Constants.WIDGET_IDS, ids);
-                context.sendBroadcast(i);
+            //start CallLoggerService
+            if (!prefs.getBoolean(Constants.PREF_OTHER[24], false))
+                context.startService(new Intent(context, CallLoggerService.class));
+            else {
+                //Update widgets
+                int[] ids = CustomApplication.getWidgetIds(Constants.CALLS);
+                if (ids.length != 0) {
+                    Intent i = new Intent(Constants.CALLS_BROADCAST_ACTION);
+                    i.putExtra(Constants.WIDGET_IDS, ids);
+                    context.sendBroadcast(i);
+                }
             }
-        }
 
-        //start WatchDogService
+            //start WatchDogService
         /*if (prefs.getBoolean(Constants.PREF_OTHER[4], true))
             context.startService(new Intent(context, WatchDogService.class));*/
 
-        //start FloatingWindow
+            //start FloatingWindow
         /*if (prefs.getBoolean(Constants.PREF_OTHER[32], false) &&
                 ((prefs.getBoolean(Constants.PREF_OTHER[41], false) && MobileUtils.isMobileDataActive(context)) ||
                         !prefs.getBoolean(Constants.PREF_OTHER[41], false)))
@@ -55,5 +58,6 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
 
         /*CustomApplication.setOnOffAlarms(context);
         CustomApplication.setCallResetAlarm(context);*/
+        }
     }
 }
